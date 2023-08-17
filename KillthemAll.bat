a2c89189503f80d2d7772e217513cd44ba9339ad
Ivan Perehynets <jonilviv@gmail.com>
2023-08-17 17:57:44 +0200

    Init commit

---
+++ KillthemAll.bat
@@ -0,0 +1,17 @@
+taskkill /f /t /im dotnet.exe
+taskkill /f /t /im devenv.exe
+taskkill /f /t /im msbuild.exe
+taskkill /f /t /im VBCSCompiler.exe
+taskkill /f /t /im ServiceHub*
+taskkill /f /t /im perfWatson2.exe
+taskkill /f /t /im StandardCollector.Service.exe
+taskkill /f /t /im Uno.SourceGeneration.Host.exe
+taskkill /f /t /im iisexpress.exe
+taskkill /f /t /im w3wp.exe
+taskkill /f /t /im node.exe
+taskkill /f /t /im adb.exe
+taskkill /f /t /im Microsoft.ServiceHub.Controller.exe
+taskkill /f /t /im node*
+taskkill /f /t /im JetBrains*
+taskkill /f /t /im ServiceHub*
+taskkill /f /t /im vsls-agent.exe
