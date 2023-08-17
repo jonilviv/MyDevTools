f17c6afacfed805c02dcc31a98495b86336b3c80
Ivan Perehynets <jonilviv@gmail.com>
2023-08-17 18:07:47 +0200

    small changes

--- KillthemAll.bat
+++ KillthemAll.bat
@@ -1,17 +1,22 @@
-taskkill /f /t /im dotnet.exe
+REM free to use in your own risc
+REM please sort applications by name
+REM !!! DO NOT RUN ON PRODUCTION !!!
+
+taskkill /f /t /im adb.exe
 taskkill /f /t /im devenv.exe
+taskkill /f /t /im dotnet.exe
+taskkill /f /t /im iisexpress.exe
+taskkill /f /t /im JetBrains*
+taskkill /f /t /im Microsoft.ServiceHub.Controller.exe
 taskkill /f /t /im msbuild.exe
-taskkill /f /t /im VBCSCompiler.exe
-taskkill /f /t /im ServiceHub*
+taskkill /f /t /im node*
 taskkill /f /t /im perfWatson2.exe
+taskkill /f /t /im ServiceHub*
 taskkill /f /t /im StandardCollector.Service.exe
 taskkill /f /t /im Uno.SourceGeneration.Host.exe
-taskkill /f /t /im iisexpress.exe
-taskkill /f /t /im w3wp.exe
-taskkill /f /t /im node.exe
-taskkill /f /t /im adb.exe
-taskkill /f /t /im Microsoft.ServiceHub.Controller.exe
-taskkill /f /t /im node*
-taskkill /f /t /im JetBrains*
-taskkill /f /t /im ServiceHub*
+taskkill /f /t /im VBCSCompiler.exe
+taskkill /f /t /im VsDebugConsole.exe
+taskkill /f /t /im VsHub*
+taskkill /f /t /im vsjitdebugger.exe
 taskkill /f /t /im vsls-agent.exe
+taskkill /f /t /im w3wp.exe
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # KillThemAll
 A bat file, that kill all Windows processes, that solve some issues on .NET developers machine.
+
+Run this file as Administrator.
+
+You can add path to this file for easy-to-use.
