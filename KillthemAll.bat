d12d2ecbba269e97ce42ceab71ec8d0ecf8a31e5
IvanP <jonilviv@gmail.com>
2024-02-12 13:12:54 +0100

    Killing in background

--- KillthemAll.bat
+++ KillthemAll.bat
@@ -2,23 +2,23 @@ REM free to use in your own risc
 REM please sort applications by name
 REM !!! DO NOT RUN ON PRODUCTION !!!
 
-taskkill /f /t /im adb.exe
-taskkill /f /t /im devenv.exe
-taskkill /f /t /im dotnet.exe
-taskkill /f /t /im iisexpress.exe
-taskkill /f /t /im JetBrains*
-taskkill /f /t /im Microsoft.ServiceHub.Controller.exe
-taskkill /f /t /im msbuild.exe
-taskkill /f /t /im node*
-taskkill /f /t /im perfWatson2.exe
-taskkill /f /t /im ServiceHub*
-taskkill /f /t /im StandardCollector.Service.exe
-taskkill /f /t /im Uno.SourceGeneration.Host.exe
-taskkill /f /t /im VBCSCompiler.exe
-taskkill /f /t /im VsDebugConsole.exe
-taskkill /f /t /im VsHub*
-taskkill /f /t /im vsjitdebugger.exe
-taskkill /f /t /im vsls-agent.exe
-taskkill /f /t /im w3wp.exe
-taskkill /f /t /im PerfWatson.exe
-taskkill /f /t /im PerfWatson2.exe
+start taskkill /f /t /im adb.exe
+start taskkill /f /t /im devenv.exe
+start taskkill /f /t /im dotnet.exe
+start taskkill /f /t /im iisexpress.exe
+start taskkill /f /t /im JetBrains*
+start taskkill /f /t /im Microsoft.ServiceHub.Controller.exe
+start taskkill /f /t /im msbuild.exe
+start taskkill /f /t /im node*
+start taskkill /f /t /im perfWatson2.exe
+start taskkill /f /t /im ServiceHub*
+start taskkill /f /t /im StandardCollector.Service.exe
+start taskkill /f /t /im Uno.SourceGeneration.Host.exe
+start taskkill /f /t /im VBCSCompiler.exe
+start taskkill /f /t /im VsDebugConsole.exe
+start taskkill /f /t /im VsHub*
+start taskkill /f /t /im vsjitdebugger.exe
+start taskkill /f /t /im vsls-agent.exe
+start taskkill /f /t /im w3wp.exe
+start taskkill /f /t /im PerfWatson.exe
+start taskkill /f /t /im PerfWatson2.exe
