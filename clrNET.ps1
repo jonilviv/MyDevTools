291e5181266e2e1cb53cc0c11ddf7271256fc610
IvanP <jonilviv@gmail.com>
2024-09-10 11:28:46 +0200

    Added killing IIS before, also fixed old Resharper folder.

--- clrNET.ps1
+++ clrNET.ps1
@@ -1,9 +1,9 @@
-taskkill /f /t /im w3wp.exe
 iisreset /STOP
+taskkill /f /t /im w3wp.exe
 nuget locals all -clear
 Get-ChildItem $Env:WinDir"\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files" -Recurse -Include "*" | Remove-Item -Force -Recurse -Verbose
 Get-ChildItem $Env:WinDir"\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files" -Recurse -Include "*" | Remove-Item -Force -Recurse -Verbose
-Get-ChildItem $Env:LOCALAPPDATA\JetBrains\Transient\ReSharperPlatformVs16 -Recurse -Include "*" | Remove-Item -Force -Recurse -Verbose
+Get-ChildItem $Env:LOCALAPPDATA\JetBrains\Transient\ReSharperPlatformVs17 -Recurse -Include "*" | Remove-Item -Force -Recurse -Verbose
 Get-ChildItem $Env:LOCALAPPDATA\JetBrains\Installations\Packages -Recurse -Include "*" | Remove-Item -Force -Recurse -Verbose
 Get-ChildItem $Env:LOCALAPPDATA\Temp -Recurse -Include "*" | Remove-Item -Force -Recurse -Verbose -ErrorAction "Ignore"
 Get-ChildItem $Env:WinDir\Temp -Recurse -Include "*" | Remove-Item -Force -Recurse -Verbose -ErrorAction "Ignore"
