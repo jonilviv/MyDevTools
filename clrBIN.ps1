5fd1cf68c5a808f73a04eae5f4f9a104ea7a0bca
IvanP <jonilviv@gmail.com>
2024-09-10 11:28:06 +0200

    Added folder `packages`

--- clrBIN.ps1
+++ clrBIN.ps1
@@ -1,4 +1,4 @@
-Get-ChildItem -Recurse -Attributes Directory -Include '.nuget','.vs','bin','obj','TestResults','Backup','MigrationBackup','BenchmarkDotNet.Artifacts','_ReSharper.Caches','node_modules','dist' | Remove-Item -Force -Recurse -Verbose
+Get-ChildItem -Recurse -Attributes Directory -Include '.nuget','.vs','bin','obj','TestResults','Backup','MigrationBackup','BenchmarkDotNet.Artifacts','_ReSharper.Caches','node_modules','dist','packages' | Remove-Item -Force -Recurse -Verbose
 Get-ChildItem -Recurse -Include '*.user','*.vspscc','*.vssscc','UpgradeLog.htm','*.orig','TestResult.xml','*.tmp','*.nupkg','*.dbmdl','*.jfm' | Remove-Item -Force -Recurse -Verbose
 
 #,'*.log'
